671e36a02ab5b20f64d039b90f2d9d5881e3af86
Ivan Zhou <ivan.zhou@makeblock.com>
2019-07-11 15:36:16 +0800

    fix: 修复翻译扩展，在部分语种下访客语言的检测结果错误问题

--- src/extensions/scratch3_translate/index.js
+++ src/extensions/scratch3_translate/index.js
@@ -177,6 +177,10 @@ class Scratch3TranslateBlocks {
      */
     getViewerLanguage () {
         this._viewerLanguageCode = this.getViewerLanguageCode();
+        // 土库曼语scratch没有对应翻译，手动写死
+        if (this._viewerLanguageCode === 'tk'){
+            return 'Тürkmençe';
+        }
         const names = languageNames.menuMap[this._viewerLanguageCode];
         let langNameObj = names.find(obj => obj.code === this._viewerLanguageCode);
 
